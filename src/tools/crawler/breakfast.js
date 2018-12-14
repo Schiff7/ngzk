895480266060dec641e19c05bd5da2023adf095c
Hu <apophisy@live.cn>
2018-12-14 22:56:28 +0800

    update crawler.

--- src/tools/crawler/breakfast.js
+++ src/tools/crawler/breakfast.js
@@ -1,12 +1,11 @@
 // breakfast.
 const Utils = require('./utils');
 const members = require('./members');
-const { replace } = require('lodash');
 
 
 function breakfast() {
   const date = Utils.format(new Date(), 'yyyyMMdd');
-  const urls = members.map(name => `http://blog.nogizaka46.com/${replace(name, /\s/, '.')}/?d=${date}`);
+  const urls = members.namespace('.').map(name => `http://blog.nogizaka46.com/${name}/?d=${date}`);
   console.log(urls);
 }
 
--- src/tools/crawler/profiles.js
+++ src/tools/crawler/profiles.js
@@ -4,17 +4,48 @@ const axios = require('axios');
 const { JSDOM } = require('jsdom');
 const Utils = require('./utils');
 const members = require('./members');
+const yaml = require('js-yaml');
+
+const fmap = (funs) => (params) => {
+  const length = funs.length;
+  const result = [];
+  if ( params.length === length ) return [];
+  for ( let i = 0; i < length; i++ ) {
+    result.push(funs[i](params[i]));
+  }
+  return result;
+}
 
 async function profiles() {
   // here i want to use the `name` before the call chain but it was apparent that i cant get the value unless i keep every state modified by 
   // function and send it to the next function. so that would be
   // the issue in function programing ? i guess..
   const links = members.reverse().namespace('').names.map(name => `http://www.nogizaka46.com/member/detail/${name}.php`);
-  for ( link of links ) {
+  const avatars_requests = [];
+  const infos = [];
+  for ( const link of links ) {
     const response = await axios({ url: link });
     const doc = JSDOM.fragment(response);
     const profile = doc.querySelector('#profile');
+    const [ img, div ] = [...profile.children];
+    avatars_requests.push(roma => {
+      Utils.retry(() => axios({ url: img.src, responseType: 'stream' })).then(res => { 
+        res.data.pipe(fs.createWriteStream(`${Utils.foldback(__dirname, 2)}/images/member/${roma}.jpg`));
+      }).catch(error => console.error(error));
+    });
+    const [ h2, dl ] = [...div.children];
+    const [ hiragana, name ] = h2.textContent.split('\n');
+    const [ birthdate, abo, constellation, stature ] = [...dl.querySelectorAll('dd')].map(dd => dd.textContent);
+    infos.push(roma => { 
+      const writable = fs.createWriteStream(`${Utils.foldback(__dirname, 2)}/views/profile/${roma}.yml`);
+      writable.write( yaml.safeDump({ name, hiragana, birthdate, abo, constellation, stature }), error => {
+        if (error) console.error(error);
+      });
+    })
   }
+
+  fmap(avatars_requests)(members.namespace('_'));
+  // fmap(infos)(members.namespace('_'));
 }
 
 profiles();
